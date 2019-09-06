fc25e61ebbef31d0363313692cefbc5638593d5b
t-ueta <ueta7651@gmail.com>
2019-09-06 20:33:45 +0900

    refactoring

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
     function buildHTML(message){
-      if ( message.image ) {
+      var mix = message.image ? `<img src=${message.image} ></img> `: "";
         var html =
           `<div class="message" data-message-id=${message.id}>
             <div class="upper-message">
@@ -16,28 +16,9 @@ $(function(){
                 ${message.content}
               </p>
             </div>
-            <img src=${message.image} >
+            ${mix}
           </div>`
         return html;
-      } else {
-        var html =
-          `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
-      };
     }
   $('#form').on('submit', function(e){
     e.preventDefault();
