1aeee7a94b5cb5f30104c085912227ade028479c
t-ueta <ueta7651@gmail.com>
2019-09-16 11:07:57 +0900

    add-timeformat

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,8 +54,6 @@ $(function () {
         data: { id: last_message_id },
         dataType: 'json'
       })
-
-
         .done(function (messages) {
           var insertHTML = '';
           messages.forEach(function (message) {
@@ -71,69 +69,3 @@ $(function () {
   };
   setInterval(reloadMessages, 5000);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(function(){
-//     function buildHTML(message){
-//       var mix = message.image ? `<img src=${message.image} > `: "";
-//         var html =
-//           `<div class="message" data-message-id=${message.id}>
-//             <div class="upper-message">
-//               <div class="upper-message__user-name">
-//                 ${message.user_name}
-//               </div>
-//               <div class="upper-message__date">
-//                 ${message.date}
-//               </div>
-//             </div>
-//             <div class="lower-message">
-//               <p class="lower-message__content">
-//                 ${message.content}
-//               </p>
-//             </div>
-//             ${mix}
-//           </div>`
-//         return html;
-//     }
-//   $('#form').on('submit', function(e){
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action');
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(data){
-//       var html = buildHTML(data);
-//       $('.messages').append(html);
-//       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-//       $('form')[0].reset();
-//     })
-//     .fail(function(){
-//       alert('error');
-//     });
-//     return false;
-//   });
-// });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,7 +1,7 @@
 json.(@message, :content)
 json.user_name @message.user.name
 json.id @message.id
-json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.date @message.created_at.to_s
 json.image @message.image.url
 
 
