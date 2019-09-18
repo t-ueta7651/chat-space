f0ae7f89e31910935a775ba0499b49e80b58184d
t-ueta <ueta7651@gmail.com>
2019-09-18 20:49:11 +0900

    fix-carrerwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ueta7651''
+  config.fog_directory  = 'ueta7651'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ueta7651'
 end
